4553eee21e2346481c8c2bdbaf1b0a5573edad56
Demonsu <245681117@qq.com>
2015-07-18 21:07:32 +0800

    remove backup

--- backup/haskell/treeprinter/DocumentOfTree.hs
+++
@@ -1,7 +0,0 @@
-TEXT "aaa" :<> (TEXT "[" :<> 
-(NEST 2 
-	(LINE :<> ((TEXT "bbbbb" :<> NIL) :<> (TEXT "," :<> (
-	LINE :<> ((TEXT "eee" :<> NIL) :<> (TEXT "," :<> (
-	LINE :<> (TEXT "ffff" :<> NIL)))))))) :<> (
-	LINE :<> TEXT "]"))
-)
